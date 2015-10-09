ce1e71f1c0377d47afaabb21502c959f64bf580f
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2015-10-09 22:17:04 +0300

    - Added CashMemo class with some function stubs to be completed later - Added Article class which will provide all information about article in cash-memo

---
+++ CashTerminal/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CashTerminal
+{
+    class Article
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public float Price { get; private set; }
+        public int MaxDiscount { get; private set; }
+        public string Measure { get; private set; }
+
+        public float Count
+        {
+            get { return Count; }
+            set
+            {
+                if (value > 0)
+                    Count = value;
+            }
+        }
+    }
+}
---
+++ CashTerminal/CashMemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace CashTerminal
+{
+    class CashMemo
+    {
+        private SerialPort _sp;
+        public  List<Article> _items;
+
+        public CashMemo(string portName)
+        {
+            _sp = new SerialPort(portName);
+            _items = new List<Article>();
+        }
+
+        public void AddArticle(Article item)
+        {
+            if (item == null) return;
+
+            _items.Add(item);
+            ItemsChanged();
+        }
+
+        public bool RemoveItem(int index)
+        {
+            if (index < 0 || index >= _items.Count) return false;
+
+            _items.RemoveAt(index);
+            ItemsChanged();
+
+            return true;
+        }
+
+        public Article GetItem()
+        {
+            // Here should be used COM port
+            throw new NotImplementedException();
+        }
+
+
+        public string[] GetHistory(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrintCheque(string outputPath = "output.txt")
+        {
+            throw new NotImplementedException();
+        }
+
+        public delegate void ItemChangedDelegate();
+
+        public event ItemChangedDelegate ItemsChanged;
+    }
+}
