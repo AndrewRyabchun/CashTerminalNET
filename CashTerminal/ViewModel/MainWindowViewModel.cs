6ee36db53eb0b4c60727cb3c91a0416595fc9213
Andrew Ryabchun <aryabchun@mail.ua>
2015-10-27 14:56:37 +0200

    Reworked structure and finished main window UI

---
+++ CashTerminal/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashTerminal.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public string UserName {
+            get { return "Пользователь: " + Environment.UserName; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
